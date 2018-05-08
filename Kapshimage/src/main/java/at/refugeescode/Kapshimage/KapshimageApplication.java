e4214239546472bb6627eca7ad20da50d128ba19
mohammmod <mohammad.sawas2@outlook.com>
2018-05-08 02:48:04 +0200

    initilaize project

---
+++ Kapshimage/src/main/java/at/refugeescode/Kapshimage/KapshimageApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.Kapshimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapshimageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapshimageApplication.class, args);
+	}
+}
